119ac98986ec9ffa8731c9b386eace61cdd05eaa
CYCING <fantasy821116@gmail.com>
2023-08-21 16:39:09 +0800

    update layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import { Providers } from "@/redux/provider";
 import LoginStatus from "@/components/login/LoginStatus";
-
 import Script from "next/script";
 import "../style/globals.css";
-// import { Inter, Noto_Sans_TC } from "next/font/google";
-// const inter = Inter({ subsets: ["latin"] });
-// const notoSansTc = Noto_Sans_TC({
-//   weight: ["100", "300", "500", "700", "900"],
-//   subsets: ["latin"],
-//   style: "normal",
-//   display: "auto",
-// });
 
 export const metadata = {
   title: "PinkStory",
@@ -26,7 +17,6 @@ export default function RootLayout({
     <Providers>
       <html lang="en">
         <LoginStatus />
-        {/* <body className={inter.className}>{children}</body> */}
         <body>{children}</body>
       </html>
     </Providers>
